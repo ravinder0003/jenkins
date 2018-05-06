cdbdfafc0cc0b173bf090fa5659e3fbb737561f2
ravinder0003 <ravinder0003@gmail.com>
2018-05-06 14:00:04 +0530

    Update and rename superspeed.sh to build_aicp.sh

---
+++ build_aicp.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+username=ravinder
+
+use_ccache="$1"
+make_clean="$2"
+device="$3"
+
+# Colors makes things beautiful
+
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+# CCACHE UMMM!!! Cooks my builds fast
+
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export USE_CCACHE=1
+export CCACHE_DIR=/home/ccache/$username
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+fi
+
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_DIR=/home/ccache/$username
+ccache -C
+export USE_CCACHE=1
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
+
+# Its Clean Time
+if [ "$make_clean" = "yes" ];
+then
+make clean && make clobber
+wait
+echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
+fi
+
+# Build ROM
+. build/envsetup.sh
+time brunch "$device"
