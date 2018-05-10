8c1eef7f37c7f0aa49c25894b9e7e172d70b289e
ravinder0003 <ravinder0003@gmail.com>
2018-05-11 02:10:42 +0530

    Update aicp_sync.sh

--- aicp_sync.sh
+++ aicp_sync.sh
@@ -5,12 +5,12 @@ username=ravinder
 mkdir aicp
 cd aicp
 
-# Syncing CosmicOS repo
 repo init -u https://github.com/AICP/platform_manifest.git -b o8.1
-repo sync -f --force-sync --no-clone-bundle -j4
+repo sync -f --force-sync --no-clone-bundle -j8
 
-# Cloning trees, vendor and kernel
 git clone https://github.com/AICP-Tissot/vendor-xiaomi-tissot.git vendor/xiaomi
 git clone https://github.com/AICP-Tissot/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
 git clone https://github.com/AICP-Tissot/device-xiaomi-msm8953-common.git device/xiaomi/msm8953-common
 git clone https://github.com/AICP-Tissot/device-xiaomi-tissot.git -b aicp device/xiaomi/tissot
+
+curl https://raw.githubusercontent.com/ravinder0003/jenkins/master/build_aicp.sh > build_aicp.sh
