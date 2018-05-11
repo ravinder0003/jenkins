86deabf236062739bf007759942b39f95c3485bb
ravinder0003 <ravinder0003@gmail.com>
2018-05-12 02:36:17 +0530

    Update rr_sync.sh

--- rr_sync.sh
+++ rr_sync.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b oreo
 repo sync -f --force-sync --no-clone-bundle -j8
 
 git clone https://gitlab.com/ravinder0003/vendor-xiaomi-tissot.git vendor/xiaomi
-git clone https://gitlab.com/ravinder0003/kernel_xiaomi_msm8953.git kernel/xiaomi/tissot
+git clone https://gitlab.com/ravinder0003/kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
 git clone https://gitlab.com/ravinder0003/device-xiaomi-msm8953-common.git device/xiaomi/msm8953-common
 git clone https://gitlab.com/ravinder0003/device-xiaomi-tissot.git -b rr device/xiaomi/tissot
 
