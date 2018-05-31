c723a40b70600e43e8fc8f08e83d20ece870a1f5
ravinder0003 <ravinder0003@gmail.com>
2018-05-31 10:37:07 +0000

    Update build_cos.sh

--- build_cos.sh
+++ build_cos.sh
@@ -5,14 +5,15 @@ username=ravinder
 device="$1"
 
 # Force Sync Repo
-repo sync -f --force-sync --no-clone-bundle -j8
+# repo sync -f --force-sync --no-clone-bundle -j8
+# make clobber
 
 rm -rf vendor/xiaomi/whyred
 rm -rf kernel/xiaomi/whyred
 rm -rf device/xiaomi/whyred
 
-git clone https://github.com/shekhawat2/android_vendor_xiaomi_whyred.git -b o vendor/xiaomi/whyred
-git clone https://github.com/shekhawat2/android_kernel_xiaomi_whyred.git -b o kernel/xiaomi/whyred
+git clone https://github.com/Whyred-Development/vendor_xiaomi_whyred.git vendor/xiaomi/whyred
+git clone https://github.com/Whyred-Development/kernel_xiaomi_whyred.git kernel/xiaomi/whyred
 git clone https://gitlab.com/ravinder0003/device_xiaomi_whyred.git -b cos device/xiaomi/whyred
 
 # Build ROM
