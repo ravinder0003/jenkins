e3ba272375097391b8aa206cdbadc37bc3c61ccd
ravinder0003 <ravinder0003@gmail.com>
2018-06-02 12:38:06 +0530

    Update cos_sync.sh

--- cos_sync.sh
+++ cos_sync.sh
@@ -5,7 +5,7 @@ username=ravinder
 mkdir cos
 cd cos
 
-repo init -u https://github.com/Cosmic-OS/platform_manifest.git -b oreo-mr1
+repo init -u https://github.com/Cosmic-OS/platform_manifest.git -b pulsar-release
 repo sync -f --force-sync --no-clone-bundle -j8
 
 curl https://raw.githubusercontent.com/ravinder0003/jenkins/master/build_cos.sh > build_cos.sh
