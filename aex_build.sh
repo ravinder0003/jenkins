fc511c71738b4d6b1e4919259306995a596f20c3
ravinder0003 <ravinder0003@gmail.com>
2018-08-01 10:26:50 +0530

    Update and rename build_aosip.sh to aex_build.sh

---
+++ aex_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# make clean && make clobber
+
+# Force Sync Repo
+# repo sync -f --force-sync --no-clone-bundle -j8
+
+# rm -rf vendor/xiaomi/whyred
+# rm -rf kernel/xiaomi/whyred
+# rm -rf device/xiaomi/whyred
+
+git clone https://gitlab.com/ravinder0003/proprietary_vendor_xiaomi.git vendor/xiaomi
+git clone https://gitlab.com/ravinder0003/kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660
+git clone https://gitlab.com/ravinder0003/device_xiaomi_whyred.git device/xiaomi/whyred
+
+# Build ROM
+. build/envsetup.sh
+lunch aosp_whyred-userdebug
+mka aex -j8
