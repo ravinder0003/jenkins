c6b16c657cc85d96280b47c85c86d82555899292
ravinder0003 <ravinder0003@gmail.com>
2018-08-01 10:50:38 +0530

    Update aex_build.sh

--- aex_build.sh
+++ aex_build.sh
@@ -10,8 +10,8 @@
 # rm -rf device/xiaomi/whyred
 
 git clone https://gitlab.com/ravinder0003/proprietary_vendor_xiaomi.git vendor/xiaomi
-git clone https://gitlab.com/ravinder0003/kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660
-git clone https://gitlab.com/ravinder0003/device_xiaomi_whyred.git -b aex device/xiaomi/whyred
+git clone https://gitlab.com/ravinder0003/android_kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660
+git clone https://gitlab.com/ravinder0003/android_device_xiaomi_whyred.git -b aex device/xiaomi/whyred
 
 # Build ROM
 . build/envsetup.sh
